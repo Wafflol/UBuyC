16e6bf412480b5c5fb5bbab6719825b4ac44f504
Ellaina <ellainading4321@gmail.com>
2024-11-18 19:13:40 -0800

    spring

--- UBuyC/src/main/java/project/marketplace/ubuyc/UbuycApplication.java
+++ UBuyC/src/main/java/project/marketplace/ubuyc/UbuycApplication.java
@@ -1,7 +1,7 @@
 package project.marketplace.ubuyc;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.context.annotation.*;
 
