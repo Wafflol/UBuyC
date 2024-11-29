657580bb2a5d6586a697475bd8b9cc7569a998de
Ethan <ethantan722@gmail.com>
2024-11-29 00:28:31 -0800

    models/registration tests

---
+++ UBuyC/src/test/java/project/marketplace/ubuyc/RegistrationTests.java
@@ -0,0 +1,38 @@
+package project.marketplace.ubuyc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Locale;
+import project.marketplace.models.User;
+
+import project.marketplace.registration.OnRegistrationCompleteEvent;
+import project.marketplace.registration.RegistrationListener;
+
+import org.checkerframework.common.util.count.report.qual.ReportReadWrite;
+import org.junit.jupiter.api.Test;
+
+public class RegistrationTests {
+
+    @Test
+    public void onRCEInit() {
+        User testUser = new User();
+        
+        OnRegistrationCompleteEvent t = new OnRegistrationCompleteEvent(testUser, Locale.US);
+
+        assertEquals(testUser, t.getUser());
+        assertEquals(Locale.US, t.getLocale());
+    }
+
+    @Test
+    public void regListenerInit() {
+
+        User testUser = new User();
+
+        OnRegistrationCompleteEvent regEvent = new OnRegistrationCompleteEvent(testUser, Locale.US);
+
+        RegistrationListener t = new RegistrationListener();
+        t.onApplicationEvent(regEvent);
+
+    }
+
+}
