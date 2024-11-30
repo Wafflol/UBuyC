f47e6418f61675e577a7b361084e865fa941825a
Peter Gao <pgao2289@gmail.com>
2024-11-29 18:51:48 -0800

    removed unused imports

--- UBuyC/src/main/java/project/marketplace/UbuycApplication.java
+++ UBuyC/src/main/java/project/marketplace/UbuycApplication.java
@@ -2,13 +2,8 @@ package project.marketplace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.context.annotation.*;
 
 @SpringBootApplication
-// @EnableAutoConfiguration
-// @Configuration
-// @ComponentScan("java")
 public class UbuycApplication {
 
 	public static void main(String[] args) {
